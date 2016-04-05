1f9479954cabd1b54b24a915c8a10075144ee08b
randy <randz_argonillo@yahoo.com>
2016-04-05 08:30:00 +0800

    initialize sessions

--- rails/sample_app/config/routes.rb
+++ rails/sample_app/config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
+  
   root to: 'static_pages#home'
   
   get 'help' => 'static_pages#help'
   get 'contact' => 'static_pages#contact'
   get 'about' => 'static_pages#about'
   get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'login' => 'sessions#destroy'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
